16f7aeb412e6bb5670433d4ac4a012b10f2b0862
wclsn <1574779697@qq.com>
2024-03-07 04:32:10 +0000

    degrade to v4.3.0 rc2

--- lakefile.lean
+++ lakefile.lean
@@ -1,17 +1,13 @@
 import Lake
 open Lake DSL
 
-package «lean_case_study» where
-  -- Settings applied to both builds and interactive editing
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
-    ⟨`pp.proofs.withType, false⟩
-  ]
-  -- add any additional package configuration options here
+package «lean_case_study» {
+  -- add package configuration options here
+}
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.3.0-rc2"
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «LeanCaseStudy» where
-  -- add any library configuration options here
+lean_lib «Lean_Case_Study» {
+  -- add library configuration options here
+}
